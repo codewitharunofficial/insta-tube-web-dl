7b3a52060ea750e9b530fd7a30c571035abed3a1
Arun <arunonwork3249@gmail.com>
2025-08-21 11:54:33 +0530

    Minor Fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer: React.FC = () => (
     <footer className='footer flex flex-row items-center justify-around p-[1rem] text-center bg-[#f5f5f5] text-lg shadow-md z-10'>
@@ -7,9 +8,10 @@ const Footer: React.FC = () => (
             href="https://github.com/codewitharunofficial"
             target="_blank"
             rel="noopener noreferrer"
-            className='ml-2 text-[#333] underline underline-offset-4 cursor-pointer hover:text-[#007bff] transition-colors duration-300'
+            className='ml-2 text-[#333] underline underline-offset-4 cursor-pointer hover:text-[#007bff] transition-colors duration-300 text-lg'
         >
-            GitHub Profile
+            <FaGithub className='inline mr-1' />
+            codewitharun
         </a>
     </footer>
 );
